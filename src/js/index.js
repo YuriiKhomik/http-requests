3f09cd2b7a8dec2ade69d092883e1c6846f4d3b8
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-28 08:41:57 +0300

    ...

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-console.log('azaz')
+// зроби запит сюди
+fetch('https://pokeapi.co/api/v2/pokemon/2')
+// коли прийде - розпарси відповідь
+.then(response=>{ 
+    return response.json();
+})
+// і коли цю відповідь опрацював - опрацюй його(якщо успішно)
+.then(pokemon=>{console.log(pokemon);
+})
+.catch(error => {
+    console.log(error)
+})
+
